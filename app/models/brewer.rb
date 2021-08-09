d5c7179fa6b01269d499ec150d41c7232c051e1c
macbook <geoffrey.dulac2@gmail.com>
2021-08-09 12:53:23 +0200

    brewers seed

--- app/models/brewer.rb
+++ app/models/brewer.rb
@@ -1,3 +1,6 @@
 class Brewer < ApplicationRecord
+    validates :name, presence: true
+    validates :city, presence: true
+
     has_many :beers
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+Brewer.destroy_all
+puts 'Brewer all destroyed !'
+
+brewers_website_scrapping_url = 'http://projet.amertume.free.fr/html/liste_brasseries.htm'
+brewers_html = URI.open(brewers_website_scrapping_url).read
+brewers_doc = Nokogiri::HTML(brewers_html)
+counter = 0
+puts 'Seeding brewers...'
+brewers_doc.search('#table1 tr').each do |element|
+    counter += 1
+    if counter > 1
+        brewer_name = element.search('td').first.text.strip.gsub("\t", "").gsub("\n", "").downcase.delete_prefix("de ").delete_prefix("du ")
+        brewer_city = element.search('td')[2].text.strip.gsub("\t", "").gsub("\n", "").downcase
+        brewer_country = 'france'
+        Brewer.new(name: brewer_name, city: brewer_city, country: brewer_country).save!
+    end
+end
