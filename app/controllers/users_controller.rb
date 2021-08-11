70e17c05b3614b91a6c592e6d594fe454a8b1b1d
macbook <geoffrey.dulac2@gmail.com>
2021-08-11 15:15:26 +0200

    raise added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     def create
         user = User.create(user_params)
+        raise
         if user.valid?
             payload = {user_id: user.id}
             token = encode_token(payload)
