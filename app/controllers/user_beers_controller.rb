3f28cce829ce8baddb519080e338d8540c408bb9
macbook <geoffrey.dulac2@gmail.com>
2021-08-24 18:46:16 +0200

    see more feat

--- app/controllers/user_beers_controller.rb
+++ app/controllers/user_beers_controller.rb
@@ -5,7 +5,7 @@ class UserBeersController < ApplicationController
     def index
         user = User.find_by(id: decoded_token[0]['user_id'])
         user_beers_array = []
-        UserBeer.where(user: user).each_with_index do |user_beer, index|
+        UserBeer.where(user: user).order("user_grade DESC").each_with_index do |user_beer, index|
             data = {}
             data["#{index}"] = user_beer.beer 
             data[:user_grade] = user_beer.user_grade
@@ -17,9 +17,10 @@ class UserBeersController < ApplicationController
 
     # /create_user_beer
     def create
-        userbeer = UserBeer.new
+        beer = Beer.find_by(name: params[:name])
+        userbeer = UserBeer.find_or_initialize_by(beer: beer)
         userbeer.user = User.find_by(id: decoded_token[0]['user_id'])
-        userbeer.beer = Beer.find_by(name: params[:name])
+        userbeer.beer = beer
         userbeer.user_grade = params[:user_grade]
         if userbeer.save!
             render json: { status: 'success' }
